4d7e7335d53e92fe25243cb09f756497df17ae19
NatanLima <natanlima_pa@hotmail.com>
2016-09-24 15:36:23 -0300

    exibindo produtos e pets na view de serviços

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -72,7 +72,7 @@ class ServicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_params
-      params.require(:service).permit(:description, :client_id, :price, :pet_id, :obs, :product_id)
+      params.require(:service).permit(:description, :customer_id, :price, :pet_id, :obs, product_ids: [])
     end
     def set_customers
       @customers = Customer.all      
